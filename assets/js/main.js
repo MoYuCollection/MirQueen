0d6a9787fec39e4089f65d2f2e4a68418c8788e7
kyumin park <kyuminpark02@gmail.com>
2022-10-09 23:17:28 -0300

    Segunda pre entrega

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,73 @@
-const service = {
-  makeUp: {
-    serviceName: "Maquillaje",
-    price: 4500,
+// Variables
+const makeUps = [
+  {
+    id: 1,
+    name: "Maquillaje de día",
+    price: 3500,
   },
-  nail: {
-    serviceName: "Uñas",
+  {
+    id: 2,
+    name: "Maquillaje de día con toques resaltados",
+    price: 4000,
+  },
+  {
+    id: 3,
+    name: "Maquillaje teatral",
+    price: 6000,
+  },
+  {
+    id: 4,
+    name: "Maquillaje para fotos",
+    price: 5500,
+  },
+  {
+    id: 5,
+    name: "Maquillaje de noche",
+    price: 5500,
+  },
+];
+
+const nails = [
+  {
+    id: 1,
+    name: "Uñas en acrílico",
+    price: 2500,
+  },
+  {
+    id: 2,
+    name: "Uñas en acabado ballerina",
+    price: 2800,
+  },
+  {
+    id: 3,
+    name: "Uñas solares",
     price: 3000,
   },
-};
+  {
+    id: 4,
+    name: "Uñas de porcelana",
+    price: 3150,
+  },
+  {
+    id: 5,
+    name: "Uñas gelificadas",
+    price: 4200,
+  },
+];
+
+const makeupNames = makeUps.map((makeUps) => makeUps.name);
+const makeupPrices = makeUps.map((makeUps) => makeUps.price);
+const makeupNamePrice = makeUps.map(
+  (makeUps) => `${makeUps.name} : $ ${makeUps.price}`
+);
+
+const nailNames = nails.map((nails) => nails.name);
+const nailPrices = nails.map((nails) => nails.price);
+const nailNamePrice = nails.map((nails) => `${nails.name} : $ ${nails.price}`);
 
 let totalPrice = 0;
+
+// Funciones
 
 function price() {
   alert("El precio total es: $" + totalPrice);
@@ -23,7 +81,19 @@ function price() {
   }
 }
 
-alert("Bienvenido a MirQueen!");
+// Funciones para extraer el precio
+
+function makeupPrice(totalPrice, id) {
+  totalPrice = makeUps.find((makeUps) => makeUps.id === id).price;
+  return totalPrice;
+}
+
+function nailPrice(totalPrice, id) {
+  totalPrice = nails.find((nails) => nails.id === id).price;
+  return totalPrice;
+}
+
+alert("Bienvenidos a MirQueen!");
 
 let option = prompt(
   "En qué te puedo ayudar? Ingresá lo que necesitas \n - Servicios\n - Precios\n - Reserva\n ESC para salir"
@@ -32,25 +102,60 @@ let option = prompt(
 while (option != "esc") {
   if (option == "servicios") {
     let servicios = prompt(
-      "Ingrese el servicio que desea comprar!\n Nuestros servicios son: \n - Maquillaje\n - Uñas\n - Ambos"
+      "Ingrese el servicio que desea comprar!\n Nuestros servicios son: \n - Maquillaje\n - Uñas"
     ).toLowerCase();
     if (servicios == "maquillaje") {
-      totalPrice = service.makeUp.price;
-      price(totalPrice);
+      let makeup = prompt(
+        `Escribe el maquillaje que desea comprar: \n Nuestros maquillajes son: \n - ${makeupNames.join(
+          "\n -"
+        )}`
+      ).toLowerCase();
+
+      if (makeup === "maquillaje de dia") {
+        totalPrice = makeupPrice(totalPrice, 1);
+        price();
+      } else if (makeup === "maquillaje de dia con toques resaltados") {
+        totalPrice = makeupPrice(totalPrice, 2);
+        price();
+      } else if (makeup === "maquillaje teatral") {
+        totalPrice = makeupPrice(totalPrice, 3);
+        price();
+      } else if (makeup === "maquillaje para fotos" || "maquillaje de noche") {
+        totalPrice = makeupPrice(totalPrice, 4);
+        price();
+      } else {
+        alert("Escribe una opción correcta!");
+      }
     } else if (servicios == "uñas") {
-      totalPrice = service.nail.price;
-      price(totalPrice);
-    } else if (servicios == "ambos") {
-      totalPrice = service.makeUp.price + service.nail.price;
-      price(totalPrice);
+      let nail = prompt(
+        `Escribe la uña que desea comprar: \n Nuestras uñas son: \n - ${nailNames.join(
+          "\n -"
+        )}`
+      ).toLowerCase();
+      if (nail === "uñas en acrilico") {
+        totalPrice = nailPrice(totalPrice, 1);
+        price();
+      } else if (nail === "uñas en acabado ballerina") {
+        totalPrice = nailPrice(totalPrice, 2);
+        price();
+      } else if (nail === "uñas solares") {
+        totalPrice = nailPrice(totalPrice, 3);
+        price();
+      } else if (nail === "uñas de porcelana") {
+        totalPrice = nailPrice(totalPrice, 4);
+        price();
+      } else if (nail === "uñas gelificadas") {
+        totalPrice = nailPrice(totalPrice, 5);
+        price();
+      } else {
+        alert("Escribe una opción correcta.");
+      }
     }
   } else if (option == "precios") {
     alert(
-      "Los precios de nuestros servicios: " +
-        "\n - Maquillaje: $" +
-        service.makeUp.price +
-        "\n - Uñas: $" +
-        service.nail.price
+      `Los precios de nuestros servicios: \n Maquillajes: \n - ${makeupNamePrice.join(
+        "\n -"
+      )} \n Uñas: \n - ${nailNamePrice.join("\n -")}`
     );
   } else if (option == "reserva") {
     let reserve = prompt(
@@ -68,25 +173,6 @@ while (option != "esc") {
     "En qué te puedo ayudar? Ingresa lo que necesitas \n - Servicios\n - Precios\n - Reserva\n ESC para salir"
   ).toLowerCase();
 }
-
-//! Swiper Slider (slider de maquillaje)
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
 
 // Responsive bar menu
 
